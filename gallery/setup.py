61921843a692cf140c69c5a0371304110ab4b4f0
tkdhmin <mdh@deepx.co.kr>
2024-07-26 15:31:47 +0000

    feat: add config setting for python package

---
+++ gallery/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MaxOptVit",
+    version="1.0.0",
+    author="Donghyun Min",
+    author_email="mdh38112@sogang.ac.kr",
+    description="A collection of python-based deep learning model implementation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tkdhmin/max_opt_vit.git",
+    project_urls={
+        "Bug Tracker": "https://github.com/tkdhmin/max_opt_vit/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "examples"},
+    packages=setuptools.find_packages(where="examples"),
+    python_requires=">= 3.8",
+    install_requires=[
+        # external dependencies
+        "numpy               == 1.22.3",
+        "sympy               >= 1.11",
+        "mock                == 4.0.3",
+        "nbformat            == 5.6.1",
+        "importlib-resources >= 5.9.0",
+        "pandas              == 1.5.1",
+    ],
+    extras_require={
+        'dev': ['pytest>=6.0'],
+    },
+)
